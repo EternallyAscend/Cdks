c559008078f4cfc8a593a66e82772807870139bb
EternallyAscend <Eternally-Ascend@Outlook.com>
2022-07-14 22:48:52 +0800

    Extend merge sort to mutli type.

--- algorithm/sort/mergeSort.c
+++ algorithm/sort/mergeSort.c
@@ -81,3 +81,91 @@ void testMergeSortPointerInt() {
     free(descend);
     free(copy);
 };
+
+void mergeSortPointer(void* data, int(*greaterThan(void*, void*)), void(*assign)(void*, void*), unsigned long long int typeSize, unsigned long long int length) {
+    mergeSortPointerCore(data, greaterThan, assign, typeSize, 0, length);
+};
+
+void* mergeSortPointerCopy(void* data, int(*greaterThan(void*, void*)), void(*assign)(void*, void*), unsigned long long int length, unsigned long long int typeSize) {
+    void* copy = malloc(typeSize * length);
+    for (unsigned long long int i = 0; i < length; i++) {
+        assign(&(copy[i]), &(data[i]));
+    }
+    mergeSortPointerCore(copy, greaterThan, assign, typeSize, 0, length);
+    return copy;
+};
+
+void mergeSortPointerCore(void* data, int(*greaterThan(void*, void*)), void(*assign)(void*, void*), unsigned long long int typeSize, unsigned long long int left, unsigned long long int right) {
+    if ((left + 1) >= right) {
+        return;
+    }
+    unsigned long long int middle = right - left;
+    middle = middle >> 1;
+    middle += left;
+    mergeSortPointerCore(data, greaterThan, assign, typeSize, left, middle);
+    mergeSortPointerCore(data, greaterThan, assign, typeSize, middle, right);
+    unsigned long long int cursor = 0;
+    unsigned long long int lc = left;
+    unsigned long long int rc = middle;
+    unsigned long long int tempSize = right - left;
+    void* temp = malloc(typeSize * tempSize);
+    void* cache = malloc(typeSize);
+    while (lc < middle && rc < right) {
+        assign(cache, &(data[lc]));
+        if (greaterThan(&(data[lc]), &(data[rc]))) {
+        assign(cache, &(data[rc]));
+        }
+        else {
+            lc++;
+        }
+        assign(&(temp[cursor++]), cache);
+    }
+    while (lc < middle) {
+        assign(&(temp[cursor++]), &(data[lc++]));
+    }
+    while (rc < middle) {
+        assign(&(temp[cursor++]), &(data[rc++]));
+    }
+    for (cursor = 0; cursor < tempSize; cursor++) {
+        assign(&(data[left + cursor]), &(temp[cursor]));
+    }
+    free(cache);
+    free(temp);
+};
+
+int exampleGreaterThan(void* left, void* right) {
+    return (int)left > (int)right;
+};
+
+void exampleAssign(void* left, void* right) {
+    left = (int)right;
+}
+
+void testMergeSortPointer() {
+    int size = 128;
+    unsigned long long int length = 128;
+    int* ascend = (int*)malloc(sizeof(int) * size);
+    int* descend = (int*)malloc(sizeof(int) * size);
+    for (unsigned long long int i = 0; i < size; i++) {
+        ascend[i] = i;
+        descend[i] = size - i;
+    }
+    mergeSortPointer(ascend, exampleGreaterThan, exampleAssign, sizeof(int), length);
+    for (unsigned long long int i = 0; i < size; i++) {
+        printf("%d ", ascend[i]);
+    }
+    printf("\n");
+    int* copy = (int*)mergeSortPointerCopy(descend, exampleGreaterThan, exampleAssign, sizeof(int), length);
+    
+    for (unsigned long long int i = 0; i < size; i++) {
+        printf("%d ", descend[i]);
+    }
+    printf("\n");
+    for (unsigned long long int i = 0; i < size; i++) {
+        printf("%d ", copy[i]);
+    }
+    printf("\n");
+    free(ascend);
+    free(descend);
+    free(copy);
+};
--- algorithm/sort/mergeSort.h
+++ algorithm/sort/mergeSort.h
@@ -9,4 +9,12 @@ void mergeSortPointerCoreInt(int* data, unsigned long long int left, unsigned lo
 
 void testMergeSortPointerInt();
 
+void mergeSortPointer(void* data, int(*greaterThan(void*, void*)), void(*assign)(void*, void*), unsigned long long int typeSize, unsigned long long int length);
+
+void* mergeSortPointerCopy(void* data, int(*greaterThan(void*, void*)), void(*assign)(void*, void*), unsigned long long int length, unsigned long long int typeSize);
+
+void mergeSortPointerCore(void* data, int(*greaterThan(void*, void*)), void(*assign)(void*, void*), unsigned long long int typeSize, unsigned long long int left, unsigned long long int right);
+
+void testMergeSortPointer();
+
 #endif
